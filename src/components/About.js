8309572373ea641d7f1570ae750de6025f544f0c
aidanch1 <aidanchen@gmail.com>
2021-03-26 13:21:34 -0700

    about v1.0

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,36 @@
 import React from "react"
+import LogoLink from "./LogoLink"
 
 function About(){
     return (
-        <h3 className="text-center">
-            I'm a 17 year old freshman at the University of Washington.<br />
-            I like coding and hope to major in CS.
-        </h3>
+        <div className="about container">
+            <div className="aboutrow row align-items-center">
+                <div className="col-md-auto">
+                    <img src="/self-picture.png" width="300px" height="100%" alt=""></img>
+                </div>
+                <div className="col">
+                    <h3>
+                        Hi!<br />
+                        My name is Aidan Chen. Right now I'm a freshman at the University of Washington.<br />
+                        I'm not in a major yet but I hope to go into CS.<br />
+                    </h3>
+                    <h4>
+                        Hobbies: 
+                        <ul>
+                            <li>Chess. Add me! 
+                                <LogoLink link="https://www.chess.com/member/potatershshh"
+                                logo="/chess.com-Logo.png"/>
+                                <LogoLink link="https://lichess.org/@/POtatershshh"
+                                logo="/lichess-Logo.png"/>
+                            </li>
+                            <li>Coding. (wow shocker!)</li>
+                            <li>Playing games. (who doesn't?) I like minecraft and league. Also big fan of board/card games.</li>
+                            <li>Doing stuff with my friends like swimming, biking, or shooting hoops.</li>
+                        </ul>
+                    </h4> 
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Home(){
-    return (
-        <h3>Hello!</h3>
-    )
-}
-
-export default Home
